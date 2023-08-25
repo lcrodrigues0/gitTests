43c73b8cc6b97e07b7a833ca9a9da1d763e962bc
lcrodrigues <leticia.c.rodrigues0@gmail.com>
2023-08-25 17:52:25 -0300

    main from HEAD kept

--- main.c
+++ main.c
@@ -2,11 +2,8 @@
 
 int main(){
 	printf("Hello, world!");
-<<<<<<< HEAD
+
 	printf("Alteração local");
-=======
-	printf("Alteração");
->>>>>>> 5170597624200f5ffd7136dbfa6437a4c4a2e578
 
 	return 0;
 }
